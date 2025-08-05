7b16be922e9098a556417ef004dd61a1dbfda7fe
Bruce Eckel <mindviewinc@gmail.com>
2025-08-05 09:30:57 -0600

    Update config.py

--- semantic_breaks/config.py
+++ semantic_breaks/config.py
@@ -3,7 +3,7 @@ Configuration constants for the semantic line breaks tool.
 """
 
 # Configuration constants
-LONG_SENTENCE_THRESHOLD = 100  # Characters - sentences longer than this get line breaks
+LONG_SENTENCE_THRESHOLD = 80  # Characters - sentences longer than this get line breaks
 
 # Conjunctions that trigger line breaks when they start the next sentence
 TRANSITION_WORDS = [
